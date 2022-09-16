3255f3f2f874e5d8c7c62e82ddbc19b27f778ad9
Andrii Hanets <>
2022-09-16 12:16:47 +0300

    Fix difference in duration labels

--- Sources/PryntTrimmerView/Trimmer/PryntTrimmerView.swift
+++ Sources/PryntTrimmerView/Trimmer/PryntTrimmerView.swift
@@ -345,6 +345,18 @@ public protocol TrimmerViewDelegate: AnyObject {
             }
             updateSelectedTime(stoppedMoving: false)
 
+            if isLeftGesture {
+                if let startTime = startTime {
+                    leftDurationView.setDuration(startTime)
+                }
+                leftDurationView.set(visable: true)
+            } else {
+                if let endTime = endTime {
+                    rightDurationView.setDuration(endTime)
+                }
+                rightDurationView.set(visable: true)
+            }
+            
         case .cancelled, .ended, .failed:
             updateSelectedTime(stoppedMoving: true)
             isLeftGesture ? leftDurationView.set(visable: false) : rightDurationView.set(visable: false)
@@ -405,6 +417,7 @@ public protocol TrimmerViewDelegate: AnyObject {
     /// The selected end time for the current asset.
     public var endTime: CMTime? {
         let endPosition = rightHandleView.frame.origin.x + assetPreview.contentOffset.x - handleWidth
+        print(getTime(from: endPosition)?.durationText)
         return getTime(from: endPosition)
     }
 
